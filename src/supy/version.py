b11f48e80065d9ad7539949b0cc5b463ede8556b
Ting Sun <sunting.05@gmail.com>
2018-12-21 09:54:05 +0000

    dropped f-string for py3.5

--- src/supy/version.py
+++ src/supy/version.py
@@ -4,4 +4,9 @@ ver_milestone = 2018
 ver_major = 12
 ver_minor = 15
 ver_remark = ''
-__version__ = f'{ver_milestone}.{ver_major}.{ver_minor}{ver_remark}'
+__version__ = '{ver_milestone}.{ver_major}.{ver_minor}{ver_remark}'.format(
+    ver_milestone=ver_milestone,
+    ver_major=ver_major,
+    ver_minor=ver_minor,
+    ver_remark=ver_remark,
+)
