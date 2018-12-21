761c4725abb8f22e35d87a60448233d55c4c1617
Ting Sun <sunting.05@gmail.com>
2018-12-21 10:07:43 +0000

    added debug info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,15 +8,18 @@
 
 # -- Path setup --------------------------------------------------------------
 
+# sys.path.insert(0, os.path.abspath('.'))
+import platform
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import platform
+import sys
+
 import supy
+
+print(r'this build is made by:', '\n', sys.version)
 
 
 # -- Project information -----------------------------------------------------
