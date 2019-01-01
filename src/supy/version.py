ebfc09fd2b218f1a850cbef51d0d2acc5bc2dcc1
Ting Sun <sunting.05@gmail.com>
2019-01-01 17:29:09 +0000

    updated version number

--- src/supy/version.py
+++ src/supy/version.py
@@ -1,9 +1,9 @@
 # version info for supy
 
 from supy_driver import __version__ as sd_ver
-ver_milestone = 2018
-ver_major = 12
-ver_minor = 31
+ver_milestone = 2019
+ver_major = 1
+ver_minor = 1
 ver_remark = ''
 __version__ = '{ver_milestone}.{ver_major}.{ver_minor}{ver_remark}'.format(
     ver_milestone=ver_milestone,
