ef79847662ecb8cfea815be36e72b0b21ed3a23e
Ting Sun <sunting.05@gmail.com>
2019-07-14 21:31:27 +0100

    added functions for calibration and calculation of: 1) OHM; 2) surface resistance of vegetation

---
+++ src/supy/util/_atm.py
@@ -0,0 +1,189 @@
+from scipy.optimize import least_squares
+import numpy as np
+import pandas as pd
+from atmosp import calculate as ac
+
+# atmospheric related utilities
+
+
+def cal_des_dta(ta, pa, dta=1.0):
+    """calculate slope of es(Ta), i.e., saturation evaporation pressure `es` as function of air temperature `ta [K]`
+
+    Parameters
+    ----------
+    ta : numeric
+        Air temperature [K]
+    pa : numeric
+        Air pressure [Pa]
+    dta : float, optional
+        change in ta for calculating that in es, by default 1.0 K
+    """
+
+    des = ac('es', p=pa, T=ta + dta/2) - ac('es', p=pa, T=ta - dta/2)
+    des_dta = des/dta
+    try:
+        # try to pack as Series
+        des_dta = pd.Series(des_dta, index=ta.index)
+    except AttributeError as ex:
+        print(ex, 'cannot pack into pd.Series')
+        pass
+    return des_dta
+
+
+def cal_rs_obs(qh, qe, ta, rh, pa):
+    """calculate surface resistance based on observations, notably turbulent fluxes
+
+    Parameters
+    ----------
+    qh : numeric
+        sensible heat flux [W m-2]
+    qe : numeric
+        latent heat flux [W m-2]
+    ta : numeric
+        air temperature [K]
+    rh : numeric
+        relative humidity [%]
+    pa : numeric
+        air pressure [Pa]
+    """
+
+    # surface resistance at water surface [s m-1]
+    rav = 50
+
+    # psychrometric constant [Pa K-1] as a function of air pressure
+    ser_gamma = 0.665e-3 * pa
+
+    # air density [kg m-3]
+    val_rho = 1.27
+
+    # heat capacity of air [J kg-1 K-1]
+    val_cp = 1005
+
+    # slope of es(Ta) curve at Ta
+    ser_des_dTa = cal_des_dta(ta, pa, dta=1.0)
+    #
+    arr_e = ac('e', p=pa, T=ta, RH=rh)
+    arr_es = ac('es', p=pa, T=ta)
+    arr_vpd = arr_es-arr_e
+    #
+    ser_rs_1 = (ser_des_dTa / ser_gamma) * (qh / qe - 1) * rav
+    ser_rs_2 = (val_rho * val_cp * arr_vpd / (ser_gamma * qe))
+    ser_rs = ser_rs_1 + ser_rs_2
+
+    try:
+        # try to pack as Series
+        ser_rs = pd.Series(ser_rs, index=ta.index)
+    except AttributeError as ex:
+        print(ex, 'cannot pack into pd.Series')
+        pass
+
+    return ser_rs
+
+
+def cal_gs_obs(qh, qe, ta, rh, pa):
+    """calculate surface conductance based on observations, notably turbulent fluxes
+
+    Parameters
+    ----------
+    qh : numeric
+        sensible heat flux [W m-2]
+    qe : numeric
+        latent heat flux [W m-2]
+    ta : numeric
+        air temperature [K]
+    rh : numeric
+        relative humidity [%]
+    pa : numeric
+        air pressure [Pa]
+    """
+    rs_obs = cal_rs_obs(qh, qe, ta, rh, pa)
+    gs_obs = 1/rs_obs
+    return gs_obs
+
+
+def cal_g_lai(lai, g1, g_max, lai_max):
+    g_lai = g_max*lai/lai_max*g1
+    return g_lai
+
+
+def cal_g_kd(kd, g2, kd_max=1200):
+    g_kd_nom = kd/(g2+kd)
+    g_kd_denom = kd_max/(g2+kd_max)
+    g_kd = g_kd_nom/g_kd_denom
+    return g_kd
+
+
+def cal_g_dq(dq, g3, g4):
+    g_dq = g3+(1-g3)*g4**dq
+    return g_dq
+
+
+def cal_g_ta(ta_c, g5, tl=-10, th=55):
+    tc = (th-g5)/(g5-tl)
+    g_ta = ((ta_c-tl)*(th-ta_c)**tc)/((g5-tl)*(th-g5)**tc)
+    return g_ta
+
+
+def cal_g_smd(smd, g6, wp=120):
+    g_smd_nom = 1-np.exp(g6*(smd-wp))
+    g_smd_denom = 1-np.exp(g6*(0-wp))
+    g_smd = g_smd_nom/g_smd_denom
+    return g_smd
+
+
+def cal_gs_mod(kd, ta_k, rh, pa, smd, lai, g_cst, g_max=30, lai_max=6):
+    # broadcast g1 – g6
+    g1, g2, g3, g4, g5, g6 = g_cst
+    # print(g1, g2, g3, g4, g5, g6)
+    # lai related
+    g_lai = g1*g_max*lai/lai_max
+    # print('g_lai', g_lai)
+
+    # kdown related
+    g_kd = cal_g_kd(kd, g2)
+    # print('g_kd', g_kd)
+    # dq related
+    # ta_k = ta_c+273.15
+    dq = ac('qvs', T=ta_k, p=pa)-ac('qv', T=ta_k, p=pa, RH=rh)
+    g_dq = cal_g_dq(dq, g3, g4)
+    # print('g_dq', g_dq)
+    # ta related
+    ta_c = ta_k - 273.15
+    g_ta = cal_g_ta(ta_c, g5)
+    # print('g_ta', g_ta)
+    # smd related
+    g_smd = cal_g_smd(smd, g6)
+    # print('g_smd', g_smd)
+    # combine pieces
+    gs = g_lai*g_kd*g_dq*g_ta*g_smd
+
+    return gs
+
+
+def calib_g(df_fc_suews, g_max=30, lai_max=6, debug=False):
+
+    df_obs = df_fc_suews.copy()
+    df_obs.pres *= 100
+    df_obs.Tair += 273.15
+
+    # function for least_square optimiser
+    def cal_prm_g(prm_g):
+        gs_obs = cal_gs_obs(df_obs.qh, df_obs.qe, df_obs.Tair,
+                            df_obs.RH, df_obs.pres)
+        gs_mod = cal_gs_mod(df_obs.kdown, df_obs.Tair,
+                            df_obs.RH, df_obs.pres, df_obs.xsmd,
+                            df_obs.lai, prm_g, g_max, lai_max)
+        resid_gs = gs_obs - gs_mod
+        resid_gs = resid_gs.dropna()
+        return resid_gs
+
+    # initial guess
+    prm_g_0 = [3.5, 200.0, 0.13, 0.7, 30.0, 0.05]
+    # calibrated model
+    res_ls = least_squares(cal_prm_g,
+                           prm_g_0,
+                           bounds=([0, 0, 0, 0, 0, 0], np.inf))
+
+    res = res_ls if debug else res_ls.x
+
+    return res
---
+++ src/supy/util/_ohm.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 17 11:13:44 2018
+
+Authors:
+George Meachim,
+Ting Sun
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from datetime import *
+import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
+import seaborn as sns
+from matplotlib import cm
+from ._plot import plot_comp, plot_day_clm
+
+
+# Linear fitting of QS, QN, deltaQN/dt (entire timeseries)
+def derive_ohm_coef(ser_QS, ser_QN):
+    """A function to linearly fit two independant variables to a dependent one.
+    Input params: QS_Ser: The dependent variable QS (Surface heat storage). Pandas Series.
+                  QN_Ser: The first independent variable (Net all wave radiation). Pandas Series.
+                  dt: The time interval with which the rate of change of QN is calculated. Float (hours).
+    Returns: a1, a2 coefficients and a3 (intercept)
+    """
+    # derive dt in hours
+    dt_hr = ser_QN.index.freq/pd.Timedelta('1H')
+
+    # Calculate difference between neighbouring QN values
+    ser_delta_QN_dt = ser_QN.diff() / dt_hr
+
+    # Drop NaNs and infinite values
+    ser_QS = ser_QS.replace([np.inf, -np.inf], np.nan).dropna(how="all")
+    ser_QN = ser_QN.loc[ser_QS.index]
+    ser_delta_QN_dt = ser_delta_QN_dt.loc[ser_QS.index]
+
+    # Create DataFrame with regression quantities and rename cols
+    frames = [ser_QS, ser_QN, ser_delta_QN_dt]
+    regression_df = pd.concat(frames, axis=1)
+    regression_df.columns = ['QS', 'QN', 'delta_QN_dt']
+
+    # Reindex after dropping NaNs
+    regression_df.reset_index(drop=True, inplace=True)
+    regression_df.fillna(regression_df.mean(), inplace=True)
+
+    feature_cols = ['QN', 'delta_QN_dt']
+
+    X = regression_df[feature_cols].replace(
+        [np.inf, -np.inf], np.nan).dropna(how="all")
+    y = regression_df.QS
+
+    lm = LinearRegression()
+    lm.fit(X, y)
+
+    a1 = lm.coef_[0]
+    a2 = lm.coef_[1]
+    a3 = lm.intercept_
+
+    return a1, a2, a3
+
+
+def replace_ohm_coeffs(df_state_init, coefs, land_cover_type):
+    """
+    This function takes as input parameters the model initial state DataFrame,
+    the new ohm coefficients as calculated by performing linear regression on
+    AMF Obs and the land cover type for which they were calculated.
+
+    Input params: df_state_init: pandas df returned by supy after running SUEWS.
+                  coefs: tuple containing new a1, a2, a3 coefficients.
+                  land_cover_type: String specifying one of seven SUEWS land cover types.
+    Returns: df_state_init_copy: A copy of df_state_init with changed ohm params.
+    """
+
+    land_cover_type_dict = {'Paved': '1', 'Buildings': '2', 'Evergreen Trees': '3',
+                            'Deciduous Trees': '4',
+                            'Grass': '5', 'Bare soil': '6', 'Water': '7'}
+    lc_index = int(land_cover_type_dict.get(land_cover_type))-1
+    # Instantiate 4x3 matrix of zeros to put old coeffs
+    coef_matrix = np.zeros((4, 3))
+    coef_matrix[:, 0] = coefs[0]
+    coef_matrix[:, 1] = coefs[1]
+    coef_matrix[:, 2] = coefs[2]
+
+    # Copy ohm_coef part of df_state_init
+    df_ohm = df_state_init.loc[:, 'ohm_coef'].copy()
+    # Reshape values into matrix form
+    values_ohm = df_ohm.values.reshape((8, 4, 3))
+    # Get ohm values corresponding to user specified land cover and assign to matrix
+    values_ohm[lc_index] = coef_matrix
+    # Place new ohm values into df_ohm
+    df_ohm.loc[:, :] = values_ohm.flatten()
+    # Make copy of df_state_init
+    df_state_init_copy = df_state_init.copy()
+    # Replace  ohm_coef part of df_state_init with new values
+    df_state_init_copy.loc[:, 'ohm_coef'] = df_ohm.values
+
+    return df_state_init_copy
+
+
+def sim_ohm(ser_qn: pd.Series, a1: float, a2: float, a3: float) -> pd.Series:
+    """Calculate QS using OHM (Objective Hysteresis Model).
+
+    Parameters
+    ----------
+    ser_qn : pd.Series
+        net all-wave radiation.
+    a1 : float
+        a1 of OHM coefficients.
+    a2 : float
+        a2 of OHM coefficients.
+    a3 : float
+        a3 of OHM coefficients.
+
+    Returns
+    -------
+    pd.Series
+        heat storage flux calculated by OHM.
+    """
+
+    # derive delta t in hour
+    try:
+        dt_hr = ser_qn.index.freq/pd.Timedelta('1h')
+    except AttributeError as ex:
+        print('frequency info is missing from input `ser_qn`')
+        sys.exit(1)
+
+    # Calculate rate of change of Net All-wave radiation
+    ser_qn_dt = ser_qn.diff() / dt_hr
+
+    # Derive QS from OBS quantities
+    ser_qs = a1 * ser_qn + a2 * ser_qn_dt + a3
+
+    return ser_qs
+
+
+def compare_heat_storage(ser_qn_obs, ser_qs_obs, a1, a2, a3):
+    """This function compares the storage heat flux calculated with AMF
+       QN and linear regression coefficients with  that output by SUEWS.
+       Input params: QN_Ser_Obs: A series of OBS net all-wave radiation.
+                     QS_Ser_SUEWS: A series of SUEWS storage heat flux values.
+                     a1, a2, a3: Linear regression coefficients from ohm_linregress_clean.
+       Returns: MPL plot of diurnal comparison, MPL plot with 1:1 line and fitted line.
+    """
+
+    # calculate qs using OHM
+    ser_qs_sim = sim_ohm(ser_qn_obs, a1, a2, a3).rename('Sim')
+
+    # re-organise obs and sim info one dataframe
+    ser_qs_obs = ser_qs_obs.rename('Obs')
+    df_qs = pd.concat([ser_qs_sim, ser_qs_obs.rename('Obs')], axis=1)
+    # Plotting
+    plot1 = plot_day_clm(df_qs)
+    plot2 = plot_comp(df_qs)
+
+    return plot1, plot2
