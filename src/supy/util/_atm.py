c9700aa858acc46ac39bfb672bf67a851df031a7
Ting Sun <sunting.05@gmail.com>
2019-07-15 12:53:09 +0100

    added documentation

--- src/supy/util/_atm.py
+++ src/supy/util/_atm.py
@@ -7,7 +7,7 @@ from atmosp import calculate as ac
 
 
 def cal_des_dta(ta, pa, dta=1.0):
-    """calculate slope of es(Ta), i.e., saturation evaporation pressure `es` as function of air temperature `ta [K]`
+    """Calculate slope of es(Ta), i.e., saturation evaporation pressure `es` as function of air temperature `ta [K]`
 
     Parameters
     ----------
@@ -31,7 +31,7 @@ def cal_des_dta(ta, pa, dta=1.0):
 
 
 def cal_rs_obs(qh, qe, ta, rh, pa):
-    """calculate surface resistance based on observations, notably turbulent fluxes
+    """Calculate surface resistance based on observations, notably turbulent fluxes.
 
     Parameters
     ----------
@@ -45,6 +45,11 @@ def cal_rs_obs(qh, qe, ta, rh, pa):
         relative humidity [%]
     pa : numeric
         air pressure [Pa]
+
+    Returns
+    -------
+    numeric
+        Surface resistance based on observations [mm s-1]
     """
 
     # surface resistance at water surface [s m-1]
@@ -81,32 +86,69 @@ def cal_rs_obs(qh, qe, ta, rh, pa):
 
 
 def cal_gs_obs(qh, qe, ta, rh, pa):
-    """calculate surface conductance based on observations, notably turbulent fluxes
+    """Calculate surface conductance based on observations, notably turbulent fluxes.
 
     Parameters
     ----------
     qh : numeric
-        sensible heat flux [W m-2]
+        Sensible heat flux [W m-2]
     qe : numeric
-        latent heat flux [W m-2]
+        Latent heat flux [W m-2]
     ta : numeric
-        air temperature [K]
+        Air temperature [K]
     rh : numeric
-        relative humidity [%]
+        Relative humidity [%]
     pa : numeric
-        air pressure [Pa]
+        Air pressure [Pa]
+
+    Returns
+    -------
+    numeric
+        Surface conductance based on observations [mm s-1]
     """
     rs_obs = cal_rs_obs(qh, qe, ta, rh, pa)
     gs_obs = 1/rs_obs
     return gs_obs
 
 
-def cal_g_lai(lai, g1, g_max, lai_max):
-    g_lai = g_max*lai/lai_max*g1
+def cal_g_lai(lai, g1, lai_max):
+    """Calculate LAI-related correction coefficient for surface conductance.
+
+    Parameters
+    ----------
+    lai : numeric
+        Leaf area index [m2 m-2]
+    g1 : numeric
+        LAI-related correction parameter [-]
+    lai_max : numeric
+        Maximum LAI [m2 m-2]
+
+    Returns
+    -------
+    numeric
+        LAI-related correction coefficient [-]
+    """
+    g_lai = lai/lai_max*g1
     return g_lai
 
 
-def cal_g_kd(kd, g2, kd_max=1200):
+def cal_g_kd(kd, g2, kd_max=1200.):
+    """Calculate solar radiation-related correction coefficient for surface conductance.
+
+    Parameters
+    ----------
+    kd : numeric
+        Incoming solar radiation [W m-2]
+    g2 : numeric
+        Solar radiation-related correction parameter [-]
+    kd_max : numeric, optional
+        Maximum incoming solar radiation [W m-2], by default 1200.
+
+    Returns
+    -------
+    numeric
+        Solar radiation-related correction coefficient [-]
+    """
     g_kd_nom = kd/(g2+kd)
     g_kd_denom = kd_max/(g2+kd_max)
     g_kd = g_kd_nom/g_kd_denom
@@ -114,29 +156,98 @@ def cal_g_kd(kd, g2, kd_max=1200):
 
 
 def cal_g_dq(dq, g3, g4):
+    """Calculate air humidity-related correction coefficient for surface conductance.
+
+    Parameters
+    ----------
+    dq : numeric
+        Specific humidity deficit [k kg-1]
+    g3 : numeric
+        Specific humidity-related correction parameter [-]
+    g4 : numeric
+        Specific humidity-related correction parameter [-]
+
+    Returns
+    -------
+    numeric
+        Air humidity-related correction coefficient
+    """
     g_dq = g3+(1-g3)*g4**dq
     return g_dq
 
 
-def cal_g_ta(ta_c, g5, tl=-10, th=55):
+def cal_g_ta(ta_c, g5, tl=-10., th=55.):
+    """Calculate air temperature-related correction coefficient for surface conductance.
+
+    Parameters
+    ----------
+    ta_c : numeric
+        Air temperature [degC]
+    g5 : numeric
+        Air temperature-related correction parameter
+    tl : numeric, optional
+        Low temperature limit [degC], by default -10.
+    th : numeric, optional
+        High temperature limit [degC], by default 55.
+
+    Returns
+    -------
+    numeric
+        Air temperature-related correction coefficient
+    """
+
+
     tc = (th-g5)/(g5-tl)
     g_ta = ((ta_c-tl)*(th-ta_c)**tc)/((g5-tl)*(th-g5)**tc)
     return g_ta
 
 
-def cal_g_smd(smd, g6, wp=120):
+def cal_g_smd(smd, g6, wp=120.):
     g_smd_nom = 1-np.exp(g6*(smd-wp))
     g_smd_denom = 1-np.exp(g6*(0-wp))
     g_smd = g_smd_nom/g_smd_denom
     return g_smd
 
 
-def cal_gs_mod(kd, ta_k, rh, pa, smd, lai, g_cst, g_max=30, lai_max=6):
+def cal_gs_mod(kd, ta_k, rh, pa, smd, lai, g_cst, g_max=30., lai_max=6.):
+    """Model surface conductance/resistance using phenology and atmospheric forcing conditions.
+
+    Parameters
+    ----------
+    kd : numeric
+        Incoming solar radiation [W m-2]
+    ta_k : numeric
+        Air temperature [K]
+    rh : numeric
+        Relative humidity [%]
+    pa : numeric
+        Air pressure
+    smd : numeric
+        Soil moisture deficit [mm]
+    lai : numeric
+        Leaf area index [m2 m-2]
+    g_cst : size-6 array
+        Parameters to determine surface conductance/resistance:
+        g1 (LAI related), g2 (solar radiation related),
+        g3 (humidity related), g4 (humidity related),
+        g5 (air temperature related),
+        g6 (soil moisture related)
+    g_max : numeric, optional
+        Maximum surface conductance [mm s-1], by default 30
+    lai_max : numeric, optional
+        Maximum LAI [m2 m-2], by default 6
+
+    Returns
+    -------
+    numeric
+        Modelled surface conductance [mm s-1]
+    """
+
     # broadcast g1 – g6
     g1, g2, g3, g4, g5, g6 = g_cst
     # print(g1, g2, g3, g4, g5, g6)
     # lai related
-    g_lai = g1*g_max*lai/lai_max
+    g_lai = cal_g_lai(lai, g1, lai_max)
     # print('g_lai', g_lai)
 
     # kdown related
@@ -154,13 +265,36 @@ def cal_gs_mod(kd, ta_k, rh, pa, smd, lai, g_cst, g_max=30, lai_max=6):
     # smd related
     g_smd = cal_g_smd(smd, g6)
     # print('g_smd', g_smd)
-    # combine pieces
-    gs = g_lai*g_kd*g_dq*g_ta*g_smd
+    # combine all corrections
+    gs_c = g_lai*g_kd*g_dq*g_ta*g_smd
+    gs = g_max*gs_c
 
     return gs
 
 
-def calib_g(df_fc_suews, g_max=30, lai_max=6, debug=False):
+def calib_g(df_fc_suews, g_max=30., lai_max=6., debug=False):
+    """Calibrate parameters for modelling surface conductance over vegetated surfaces.
+
+    Parameters
+    ----------
+    df_fc_suews : pandas.DataFrame
+        DataFrame in `SuPy forcing <https://supy.readthedocs.io/en/latest/data-structure/df_forcing.html>`_ format
+    g_max : numeric, optional
+        Maximum surface conductance [mm s-1], by default 30
+    lai_max : numeric, optional
+        Maximum LAI [m2 m-2], by default 6
+    debug : bool, optional
+        Option to output final calibrated `scipy.least_squares`, by default False
+
+    Returns
+    -------
+    `numpy.array`, or `scipy.least_squares` if `debug==True`
+        Calibrated parameters for surface conductance:
+        g1 (LAI related), g2 (solar radiation related),
+        g3 (humidity related), g4 (humidity related),
+        g5 (air temperature related),
+        g6 (soil moisture related)
+    """
 
     df_obs = df_fc_suews.copy()
     df_obs.pres *= 100
