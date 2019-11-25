b3f86b7d0a069bc2e8bde04d2544a51b1255f1e9
Ting Sun <sunting.05@gmail.com>
2019-11-25 23:13:30 +0000

    add comments in logger part

--- src/supy/_env.py
+++ src/supy/_env.py
@@ -12,9 +12,11 @@ import tempfile
 # define local path for loading resources in this package
 path_supy_module = Path(inspect.getsourcefile(lambda: 0)).resolve().parent
 
-# set up logger
+# set up logger format, note `u` to guarantee UTF-8 encoding
 FORMATTER = logging.Formatter(
     u"%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+
+# log file name
 LOG_FILE = "SuPy.log"
 
 
